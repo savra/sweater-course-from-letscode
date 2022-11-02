52618236cb9af0a735d4ee890ec8132df285ba86
Sergey Savrukhin <savra.sv@yandex.ru>
2022-11-02 18:53:40 +0300

    initial commit

---
+++ src/main/java/com/hvdbs/sweater/SweaterCourseFromLetscodeApplication.java
@@ -0,0 +1,13 @@
+package com.hvdbs.sweater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweaterCourseFromLetscodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweaterCourseFromLetscodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hvdbs/sweater/SweaterCourseFromLetscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvdbs.sweater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweaterCourseFromLetscodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
